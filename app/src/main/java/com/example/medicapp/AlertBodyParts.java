7cd1642dd10609cd9318f397f6c7c3662bd63578
mendimLAJQI <elv-mendim.ljq@eduge.ch>
2021-03-03 12:10:54 +0100

    Avancement application page alert

---
+++ app/src/main/java/com/example/medicapp/AlertBodyParts.java
@@ -0,0 +1,14 @@
+package com.example.medicapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AlertBodyParts extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alert_body_parts);
+    }
+}
--- app/src/main/java/com/example/medicapp/MainActivity.java
+++ app/src/main/java/com/example/medicapp/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -21,4 +22,18 @@ public class MainActivity extends AppCompatActivity {
         startActivity(new Intent(getApplicationContext(),Login.class));
         finish();
     }
+
+
+
+    public void bodyparts(View view) {
+        Button btn = (Button)findViewById(R.id.btnpagealert);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, AlertBodyParts.class));
+            }
+        });
+    }
+
 }
