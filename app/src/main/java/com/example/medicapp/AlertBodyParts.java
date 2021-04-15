ebf023e062b1d6aac7c572fea2b7060654fd1216
mendimLAJQI <elv-mendim.ljq@eduge.ch>
2021-04-15 19:00:54 +0200

    Commit fin de projet pour l'école

--- app/src/main/java/com/example/medicapp/AlertBodyParts.java
+++ app/src/main/java/com/example/medicapp/AlertBodyParts.java
@@ -1,6 +1,7 @@
 package com.example.medicapp;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
@@ -21,15 +22,27 @@ import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 
 import java.io.IOException;
 import java.util.List;
 import java.util.Locale;
 
 public class AlertBodyParts extends AppCompatActivity {
-    ImageView img,img2,img3,img4;
+    ImageView img, img2, img3, img4;
     TextView textView1, textView2, textView3, textView4, textView5, t;
     FusedLocationProviderClient fusedLocationProviderClient;
+
+    FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
+    String userId;
+    String fullname;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,13 +56,26 @@ public class AlertBodyParts extends AppCompatActivity {
         textView3 = findViewById(R.id.text_view3);
         textView4 = findViewById(R.id.text_view4);
         textView5 = findViewById(R.id.text_view5);
-        img=findViewById(R.id.coeur);
-        img2=findViewById(R.id.poumons);
-        img3=findViewById(R.id.cerveau);
-        img4=findViewById(R.id.yeux);
+        img = findViewById(R.id.coeur);
+        img2 = findViewById(R.id.poumons);
+        img3 = findViewById(R.id.cerveau);
+        img4 = findViewById(R.id.yeux);
+
+        fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
+
+        userId = fAuth.getCurrentUser().getUid();
 
 
+        DocumentReference documentReference = fStore.collection("users").document(userId);
+        documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException error) {
+                fullname = (documentSnapshot.getString("userName"));
+                System.out.println(documentSnapshot.getString("userName"));
 
+            }
+        });
 
 
         ActivityCompat.requestPermissions(AlertBodyParts.this, new String[]{Manifest.permission.SEND_SMS}, PackageManager.PERMISSION_GRANTED);
@@ -60,29 +86,12 @@ public class AlertBodyParts extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if (ActivityCompat.checkSelfPermission(AlertBodyParts.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-//                    quand la permission est donnée
-//                    String phoneNumber = "0798590172";
-//                     message = getLocation();
-//                     mess1 = displayToast(getString(R.string.coeur));
-////
-////                    SmsManager smsManager = SmsManager.getDefault();
-////                    SmsManager.sendText(phoneNumber,null,message,null,null);
-//
-//                    try {
-//                        SmsManager smsManager = SmsManager.getDefault();
-//                        smsManager.sendTextMessage(phoneNumber, null, message, null, null);
-//                        Toast.makeText(getApplicationContext(), "SMS Sent!",
-//                                Toast.LENGTH_SHORT).show();
-//                    } catch (Exception e) {
-//                        Toast.makeText(getApplicationContext(),
-//                                "SMS faild, please try again later!",
-//                                Toast.LENGTH_SHORT).show();
-//                        e.printStackTrace();
-//
-//                    }
+////                    quand la permission est donnée
 
-                    getLocation();
-                    displayToast(getString(R.string.coeur));
+
+                    getLocation(getString(R.string.coeur), fullname);
+
+                    //displayToast(getString(R.string.coeur));
 
                 } else {
                     //quand la permission est refusée
@@ -100,8 +109,10 @@ public class AlertBodyParts extends AppCompatActivity {
             public void onClick(View v) {
                 if (ActivityCompat.checkSelfPermission(AlertBodyParts.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
                     //quand la permission est donnée
-                    getLocation();
-                    displayToast(getString(R.string.poumons));
+
+
+                    getLocation(getString(R.string.poumons), fullname);
+                    //displayToast(getString(R.string.poumons));
                 } else {
                     //quand la permission est refusée
                     ActivityCompat.requestPermissions(AlertBodyParts.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 44);
@@ -118,8 +129,10 @@ public class AlertBodyParts extends AppCompatActivity {
             public void onClick(View v) {
                 if (ActivityCompat.checkSelfPermission(AlertBodyParts.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
                     //quand la permission est donnée
-                    getLocation();
-                    displayToast(getString(R.string.brain));
+
+
+                    getLocation(getString(R.string.brain), fullname);
+                    //displayToast(getString(R.string.brain));
                 } else {
                     //quand la permission est refusée
                     ActivityCompat.requestPermissions(AlertBodyParts.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 44);
@@ -136,8 +149,10 @@ public class AlertBodyParts extends AppCompatActivity {
             public void onClick(View v) {
                 if (ActivityCompat.checkSelfPermission(AlertBodyParts.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
                     //quand la permission est donnée
-                    getLocation();
-                    displayToast(getString(R.string.yeux));
+
+
+                    getLocation(getString(R.string.yeux), fullname);
+                    //displayToast(getString(R.string.yeux));
                 } else {
                     //quand la permission est refusée
                     ActivityCompat.requestPermissions(AlertBodyParts.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 44);
@@ -151,9 +166,67 @@ public class AlertBodyParts extends AppCompatActivity {
     }
 
 
+//    private void getLocation() {
+//        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+//            // TODO: Consider calling
+//            //    ActivityCompat#requestPermissions
+//            // here to request the missing permissions, and then overriding
+//            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+//            //                                          int[] grantResults)
+//            // to handle the case where the user grants the permission. See the documentation
+//            // for ActivityCompat#requestPermissions for more details.
+//            //return;
+//            return;
+//        }
+//        fusedLocationProviderClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
+//            @Override
+//            public void onComplete(@NonNull Task<Location> task) {
+//                Location location = task.getResult();
+//                if (location != null) {
+//                    try {
+//                        //initialiser geocoder
+//                        Geocoder geocoder = new Geocoder(AlertBodyParts.this, Locale.getDefault());
+//                        //initialiser address list
+//                        List<Address> addresses = geocoder.getFromLocation(location.getLatitude(),location.getLongitude(),1);
+//
+//                        //set latitude sur la textView
+//                        textView1.setText(Html.fromHtml(
+//                                "<font color='#FFFFFF'><b>Latitude :</b><br></font>"
+//                                + addresses.get(0).getLatitude()
+//                        ));
+//                        //set longitude sur la text view
+//                        textView2.setText(Html.fromHtml(
+//                                "<font color='#FFFFFF'><b>Longitude :</b><br></font>"
+//                                        + addresses.get(0).getLongitude()
+//                        ));
+//                        //set nom du pays sur la text view
+//                        textView3.setText(Html.fromHtml(
+//                                "<font color='#FFFFFF'><b>Country Name :</b><br></font>"
+//                                        + addresses.get(0).getCountryName()
+//                        ));
+//                        //set localité
+//                        textView4.setText(Html.fromHtml(
+//                                "<font color='#FFFFFF'><b>Locality :</b><br></font>"
+//                                        + addresses.get(0).getLocality()
+//                        ));
+//                        //set address
+//                        textView5.setText(Html.fromHtml(
+//                                "<font color='#FFFFFF'><b>Address :</b><br></font>"
+//                                        + addresses.get(0).getAddressLine(0)
+//                        )
+//
+//                        );
+//
+//
+//                    } catch (IOException e) {
+//                        e.printStackTrace();
+//                    }
+//                }
+//            }
+//        });
 
 
-    private void getLocation() {
+    private void getLocation(final String message, final String fullname) {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
             //    ActivityCompat#requestPermissions
@@ -173,11 +246,11 @@ public class AlertBodyParts extends AppCompatActivity {
                         //initialiser geocoder
                         Geocoder geocoder = new Geocoder(AlertBodyParts.this, Locale.getDefault());
                         //initialiser address list
-                        List<Address> addresses = geocoder.getFromLocation(location.getLatitude(),location.getLongitude(),1);
+                        List<Address> addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
                         //set latitude sur la textView
                         textView1.setText(Html.fromHtml(
                                 "<font color='#FFFFFF'><b>Latitude :</b><br></font>"
-                                + addresses.get(0).getLatitude()
+                                        + addresses.get(0).getLatitude()
                         ));
                         //set longitude sur la text view
                         textView2.setText(Html.fromHtml(
@@ -198,10 +271,38 @@ public class AlertBodyParts extends AppCompatActivity {
                         textView5.setText(Html.fromHtml(
                                 "<font color='#FFFFFF'><b>Address :</b><br></font>"
                                         + addresses.get(0).getAddressLine(0)
-                        )
+                        ));
 
-                        );
+                        Address address = addresses.get(0);
 
+                        StringBuilder builder = new StringBuilder();
+                        builder.append(fullname).append(" ");
+                        builder.append(message).append(" ");
+                        builder.append(address.getLatitude()).append(" ");
+                        builder.append(address.getLongitude()).append(" ");
+                        builder.append(address.getCountryName()).append(" ");
+                        builder.append(address.getLocality()).append(" ");
+                        builder.append(address.getAddressLine(0)).append(" ");
+
+
+                        String message = builder.toString();
+                        System.out.println(message);
+
+                        String phoneNumber = "0787056891";
+
+
+                        try {
+                            SmsManager smsManager = SmsManager.getDefault();
+                            smsManager.sendTextMessage(phoneNumber, null, message, null, null);
+                            Toast.makeText(getApplicationContext(), "SMS Sent!",
+                                    Toast.LENGTH_SHORT).show();
+                        } catch (Exception e) {
+                            Toast.makeText(getApplicationContext(),
+                                    "SMS faild, please try again later!",
+                                    Toast.LENGTH_SHORT).show();
+                            e.printStackTrace();
+
+                        }
 
                     } catch (IOException e) {
                         e.printStackTrace();
--- app/src/main/java/com/example/medicapp/MainActivity.java
+++ app/src/main/java/com/example/medicapp/MainActivity.java
@@ -64,6 +64,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, AlertBodyParts.class));
+
             }
         });
     }
